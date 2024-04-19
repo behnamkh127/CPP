b4766eeb49660f78bf5094ca4117187274a922eb
bekhodad <bekhodad@student.42wolfsburg.de>
2024-04-19 09:53:55 +0200

    Delete unnecessary files and update Makefile and headers

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: bekhodad <bekhodad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 18:20:38 by bekhodad          #+#    #+#             */
-/*   Updated: 2024/04/18 14:21:56 by bekhodad         ###   ########.fr       */
+/*   Updated: 2024/04/18 14:27:00 by bekhodad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie*	zombieHorde(int N, std::string name){
 	Zombie* newZombie = new Zombie[N];
 	for(int i = 0; i < N; i++){
 		std::string newName = name;
-		newName.push_back('0' + i); // not in c++98
+		newName.push_back('0' + i);
 		newZombie[i].setName(newName);
 	}
 	return newZombie;
