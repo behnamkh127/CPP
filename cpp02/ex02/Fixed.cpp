061d5f68712b963d7ced7552fff451f049c3875d
bekhodad <bekhodad@student.42wolfsburg.de>
2024-04-29 16:59:56 +0200

    bale

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,164 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bekhodad <bekhodad@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 17:02:39 by bekhodad          #+#    #+#             */
+/*   Updated: 2024/04/29 16:58:29 by bekhodad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::_fb = 8;
+/* ************************************************************************** */
+
+Fixed::Fixed() : _fpn(0) {std::cout << "Default constractor called\n";}
+/* ************************************************************************** */
+
+Fixed::Fixed(int const num) : _fpn(num << _fb){std::cout << "Int constractor called\n";}
+/* ************************************************************************** */
+
+Fixed::Fixed(float const num) : _fpn(roundf(num * (1 << _fb))) {std::cout << "Float constractor called\n";}
+/* ************************************************************************** */
+Fixed::Fixed(const Fixed& other){
+	std::cout << "Copy constractor called\n";
+	operator=(other);
+}
+/* ************************************************************************** */
+
+Fixed& Fixed::operator=(const Fixed& other){
+	std::cout << "Copy assignment operator called\n";
+	if (this != &other) {
+		_fpn = other._fpn;
+	}
+	return *this;
+}
+/* ************************************************************************** */
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+	os << fixed.toFloat();
+	return os;
+}
+/* ************************************************************************** */
+
+float Fixed::toFloat(void) const {
+	return static_cast<float>(_fpn) / (1 << _fb);
+}
+/* ************************************************************************** */
+
+bool	Fixed::operator!=(const Fixed& other)const{
+	return this->toFloat() != other.toFloat();
+}
+/* ************************************************************************** */
+
+bool	Fixed::operator>(const Fixed& other)const{
+	return this->toFloat() > other.toFloat();
+}
+/* ************************************************************************** */
+
+bool	Fixed::operator==(const Fixed& other)const{
+	return this->toFloat() == other.toFloat();
+}
+/* ************************************************************************** */
+
+bool	Fixed::operator>=(const Fixed& other)const{
+	return this->toFloat() >= other.toFloat();
+}
+/* ************************************************************************** */
+
+bool	Fixed::operator<=(const Fixed& other)const{
+	return this->toFloat() <= other.toFloat();
+}
+/* ************************************************************************** */
+
+bool	Fixed::operator<(const Fixed& other)const{
+	return this->toFloat() < other.toFloat();
+}
+/* ************************************************************************** */
+
+Fixed	Fixed::operator+(const Fixed& other)const{
+	return this->toFloat() + other.toFloat();
+}
+/* ************************************************************************** */
+
+Fixed	Fixed::operator-(const Fixed& other)const{
+	return this->toFloat() - other.toFloat();
+}
+/* ************************************************************************** */
+
+Fixed	Fixed::operator*(const Fixed& other)const{
+	return this->toFloat() * other.toFloat();
+}
+/* ************************************************************************** */
+
+Fixed	Fixed::operator/(const Fixed& other)const{
+	return this->toFloat() / other.toFloat();
+}
+/* ************************************************************************** */
+
+Fixed&	Fixed::operator++(){
+	_fpn++;
+	return *this;
+}
+/* ************************************************************************** */
+
+Fixed&	Fixed::operator--(){
+	_fpn--;
+	return *this;
+}
+/* ************************************************************************** */
+
+Fixed 	Fixed::operator++(int){
+	Fixed temp(*this);
+	_fpn++;
+	return temp;
+}
+/* ************************************************************************** */
+
+Fixed 	Fixed::operator--(int){
+	Fixed temp(*this);
+	_fpn--;
+	return temp;
+}
+/* ************************************************************************** */
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b){
+	if(a._fpn < b._fpn)
+		return a;
+	else
+		return b;
+}
+/* ************************************************************************** */
+
+Fixed& Fixed::min(Fixed& a, Fixed& b){
+	if(a._fpn < b._fpn)
+		return a;
+	else
+		return b;
+}
+/* ************************************************************************** */
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b){
+	if(a._fpn > b._fpn)
+		return a;
+	else
+		return b;
+}
+/* ************************************************************************** */
+
+Fixed& Fixed::max(Fixed& a, Fixed& b){
+	if(a._fpn < b._fpn)
+		return a;
+	else
+		return b;
+}
+/* ************************************************************************** */
+
+int Fixed::toInt(void) const {
+	return _fpn >> _fb;
+}
+
+Fixed::~Fixed() {std::cout << "Destructor called\n";}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bekhodad <bekhodad@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 13:50:16 by bekhodad          #+#    #+#             */
+/*   Updated: 2024/04/29 16:20:12 by bekhodad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class Fixed{
+	public:
+		Fixed();
+		Fixed(int const num);	
+		Fixed(float const num);
+		Fixed(const Fixed& other);
+		Fixed&	operator=(const Fixed& other);
+		bool	operator==(const Fixed& other) const;
+		bool	operator!=(const Fixed& other) const;
+		bool	operator<(const Fixed& other) const;
+		bool	operator>(const Fixed& other) const;
+		bool	operator<=(const Fixed& other) const;
+		bool	operator>=(const Fixed& other) const;
+		Fixed	operator+(const Fixed& other) const;
+		Fixed	operator-(const Fixed& other) const;
+		Fixed	operator*(const Fixed& other) const;
+		Fixed	operator/(const Fixed& other) const;
+		Fixed&	operator++();
+		Fixed	operator++(int);
+		Fixed	operator--(int);
+		Fixed&	operator--();
+		static const Fixed& min(const Fixed& a, const Fixed& b);
+		static Fixed& min(Fixed& a, Fixed& b);
+		static const Fixed& max(const Fixed& a, const Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		float	toFloat(void)const;
+		int		toInt(void)const;
+		~Fixed();
+	private:
+		int _fpn;
+		static const int _fb;
+};
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
